5192ce29df251e9cebe54fae628b293871fe955a
Dan The Man <dan.attard@gmail.com>
2023-07-21 01:42:34 -0400

    dan stuff

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
+#    st.session_state.openai_key = "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"  # Hard-coded OpenAI key
 import streamlit as st
 import pandas as pd
 from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 import matplotlib.pyplot as plt
 
-st.title("pandas-ai streamlit interface")
-
-st.write("A demo interface for [PandasAI](https://github.com/gventuri/pandas-ai)")
-st.write(
-    "Looking for an example *.csv-file?, check [here](https://gist.github.com/netj/8836201)."
-)
+st.write("Dan's Streamlit [PandasAI](https://github.com/gventuri/pandas-ai)")
 
 if "openai_key" not in st.session_state:
     with st.form("API key"):
-        key = st.text_input("OpenAI Key", value="", type="password")
+        key = st.text_input("OpenAI Key", value="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")
         if st.form_submit_button("Submit"):
             st.session_state.openai_key = key
             st.session_state.prompt_history = []
@@ -22,7 +18,7 @@ if "openai_key" not in st.session_state:
 if "openai_key" in st.session_state:
     if st.session_state.df is None:
         uploaded_file = st.file_uploader(
-            "Choose a CSV file. This should be in long format (one datapoint per row).",
+            "Choose CSV...",
             type="csv",
         )
         if uploaded_file is not None:
@@ -30,7 +26,7 @@ if "openai_key" in st.session_state:
             st.session_state.df = df
 
     with st.form("Question"):
-        question = st.text_input("Question", value="", type="default")
+        question = st.text_input("Question", value="How many rows?")
         submitted = st.form_submit_button("Submit")
         if submitted:
             with st.spinner():
@@ -45,12 +41,11 @@ if "openai_key" in st.session_state:
                 st.session_state.prompt_history.append(question)
 
     if st.session_state.df is not None:
-        st.subheader("Current dataframe:")
+        #st.subheader("Dataframe:")
         st.write(st.session_state.df)
 
-    st.subheader("Prompt history:")
+    #st.subheader("History:")
     st.write(st.session_state.prompt_history)
-
 
 if st.button("Clear"):
     st.session_state.prompt_history = []
